b6fc52bc2ad9a1867364afeece9d1f94e6bd859d
Mathieu Trudeau <mathieutrudeau11@gmail.com>
2023-10-17 16:00:36 -0400

    Added Part 1 for asg2

--- src/MainDriver.cpp
+++ src/MainDriver.cpp
@@ -3,16 +3,35 @@
 #include "Orders/OrdersDriver.h"
 #include "Cards/CardsDriver.h"
 #include "Player/PlayerDriver.h"
-
+#include "CommandProcessing/CommandProcessingDriver.h"
+#include <regex>
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-  testLoadMaps();
-  testGameStates();
-  testOrdersLists();
-  testCards();
-  testPlayers();
-  return 0;
+    bool runTests = false;
+    bool useFileCmd = false;
+    string cmdFilename = "";
+
+    for (int i = 0; i < argc; ++i) {
+        if (regex_match((std::string)argv[i],regex ("^-[tT][eE][sS][tT]\\s*$")))
+            runTests = true;
+        if (regex_match((std::string)argv[i],regex ("^-[fF][iI][lL][eE]\\s*$"))){
+            useFileCmd=true;
+            cmdFilename = ((std::string)argv[i+1]);
+        }
+    }
+
+
+    if(runTests){
+        testCommandProcessor();
+        testLoadMaps();
+        testGameStates();
+        testOrdersLists();
+        testCards();
+        testPlayers();
+    }
+
+    return 0;
 }
 
