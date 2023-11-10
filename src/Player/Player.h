d3e1611557989cc967eb41a32e6228d14298cf36
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2023-11-10 16:02:41 -0500

    Update Player.h
    added comments

--- src/Player/Player.h
+++ src/Player/Player.h
@@ -20,6 +20,13 @@ class Bomb;
 class Blockade;
 class Negotiate;
 
+/**
+ * @class Player
+ * @brief Represents a player in the game.
+ *
+ * This class is responsible for managing the player's state,
+ * including their territories, cards, orders, and game phase.
+ */
 class Player {
 private:
   std::string phase;
@@ -38,13 +45,31 @@ public:
   // --------------------------------
   // Constructors
   // --------------------------------
+   /**
+   * @brief Constructs a new Player object.
+   * @param game The game engine instance.
+   * @param cards The player's hand of cards.
+   * @param name The name of the player.
+   */
   Player(GameEngine* game, Hand* cards, std::string name);
+  /**
+   * @brief Destructor for the Player class.
+   */
   ~Player();
+  /**
+   * @brief Copy constructor for the Player class.
+   * @param p The Player object to be copied.
+   */
   Player(const Player &p) = default;
 
   // --------------------------------
   // Operator Overloads
   // --------------------------------
+   /**
+   * @brief Overloads the assignment operator.
+   * @param other The Player object to assign from.
+   * @return Reference to the updated Player object.
+   */
   Player& operator=(const Player &other);
 
   // --------------------------------
@@ -61,6 +86,10 @@ public:
   void addReinforcement(int reinforcement);
   int getContinentBonus();
   Territory* findFirstNeighbourTerritory(Territory* target);
+    /**
+   * @brief Gets the list of enemy players.
+   * @return Vector of pointers to Player objects representing enemies.
+   */
   std::vector<Player*> getEnemies();
 
   // --------------------------------
@@ -97,6 +126,18 @@ public:
 
 
 public:
+
+  /**
+   * @brief Overloads the stream insertion operator for the Player class.
+   * @param out The output stream.
+   * @param player The Player object to be inserted into the stream.
+   * @return Reference to the output stream.
+   */
   friend std::ostream& operator <<(std::ostream &out, const Player &player);
+    /**
+   * @brief Determines if the player can attack another player.
+   * @param pPlayer Pointer to the Player object to potentially attack.
+   * @return True if the player can attack, false otherwise.
+   */
   bool canAttack(Player *pPlayer);
 };
